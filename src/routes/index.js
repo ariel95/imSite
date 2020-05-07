31f240ff2545aafe05bf90077004f374bc1222b6
arielvergarautn <arie@est.frba.utn.edu.ar>
2020-05-07 08:32:38 -0300

    saving images in cloudinary from the proyect

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,36 @@
 const { Router } = require('express');
 const router = Router();
 
+const Photo = require('../models/Photo');
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+const fs = require('fs-extra');
+
 router.get('/', (req,res) => {
     res.render('images');
 });
 router.get('/images/add', (req,res) => {
     res.render('image_form');
 });
+router.post('/images/add', async (req,res) =>{
+    const {title, description} = req.body;
+    const result = await cloudinary.v2.uploader.upload(req.file.path); // Async 
+    console.log(result);
+    const newPhoto = new Photo({
+        title,
+        description,
+        imageUrl: result.url,
+        public_id: result.public_id
+    }) 
+    await newPhoto.save();
+    await fs.unlink(req.file.path); //erase the file from the server
 
+    res.send('recived');
+})
 module.exports = router;
